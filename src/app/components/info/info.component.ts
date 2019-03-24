cda2c1318800ff818d2190a90766cd9110385480
Florian Gössele <moin@frot.io>
2019-03-24 15:12:48 +0100

    Exchangedn Info and Help Component

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -1,29 +1,8 @@
 import { Component } from '@angular/core';
-import { SwPush } from '@angular/service-worker';
-import { PushNotificationService } from './../../services/push-notification/push-notification.service';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.styl']
 })
-export class InfoComponent {
-  // from https://blog.angular-university.io/angular-push-notifications/
-  readonly VAPID_PUBLIC_KEY = 'BBqTVCz44SYRYuFLopkKdG_rT2izyEMsRbZmluXBAVYM25TmQvVz3tTd18a-02GZmiiYBUZHGq7LmD5qGbs0mMo';
-
-  constructor(
-    private swPush: SwPush,
-    private pushNotificationService: PushNotificationService) {}
-
-  subscribeToNotifications() {
-    this.swPush.requestSubscription({
-      serverPublicKey: this.VAPID_PUBLIC_KEY
-    })
-    .then(sub => this.pushNotificationService.addPushSubscriber(sub).subscribe())
-    .catch(err => alert('Could not subscribe to notifications'));
-  }
-
-  sendNotification() {
-    this.pushNotificationService.send().subscribe();
-  }
-}
+export class InfoComponent {}
