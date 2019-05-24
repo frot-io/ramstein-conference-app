b642284ea3956374015b86b4023d0697e69d3e3f
Florian Gössele <moin@frot.io>
2019-05-24 16:04:18 +0200

    Added locations to leaflet

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,13 +7,33 @@ import { tileLayer, latLng } from 'leaflet';
   styleUrls: ['./map.component.styl']
 })
 export class MapComponent {
-  latitude = 49.4684328;
-  longitude = 7.5256019;
-  options = {
+  // Defaults
+  private defaultLat = 49.4684328;
+  private defaultLng = 7.6256019;
+  public options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '© OpenStreetMap contributors' })
     ],
-    zoom: 15,
-    center: latLng(this.latitude, this.longitude)
+    zoom: 11,
+    center: latLng(this.defaultLat, this.defaultLng)
   };
+
+  // Locations
+  private locationIcon = icon({
+    iconSize: [ 25, 41 ],
+    iconAnchor: [ 13, 16 ],
+    iconUrl: 'assets/leaflet/marker-icon.png',
+    shadowUrl: 'assets/leaflet/marker-shadow.png'
+  });
+  private locationOptions = { icon: this.locationIcon };
+  public locations = [
+    marker([ 49.468336, 7.525594 ], this.locationOptions).bindPopup('Friedenscamp'),
+    marker([ 49.509176, 7.780170 ], this.locationOptions).bindPopup('Werner Liebrich Friedensfußballturnier'),
+    marker([ 49.468336, 7.525594 ], this.locationOptions).bindPopup('Friedenswerkstatt'),
+    marker([ 49.444297, 7.761579 ], this.locationOptions).bindPopup('Anti-Basen Kongress'),
+    marker([ 49.444297, 7.761579 ], this.locationOptions).bindPopup('Abendveranstaltung'),
+    marker([ 49.447888, 7.555465 ], this.locationOptions).bindPopup('Demonstration und Festival Aufakt'),
+    marker([ 49.438392, 7.568276 ], this.locationOptions).bindPopup('Abschluß + Festival'),
+    marker([ 49.468336, 7.525594 ], this.locationOptions).bindPopup('Friedensfest im Camp'),
+  ];
 }
