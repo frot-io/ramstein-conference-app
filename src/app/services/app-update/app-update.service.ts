bed78d7fb4ed1b64117c709f88d0e904a030280f
Florian Gössele <moin@frot.io>
2019-06-16 20:54:56 +0200

    Added snack bar

--- src/app/services/app-update/app-update.service.ts
+++ src/app/services/app-update/app-update.service.ts
@@ -2,14 +2,18 @@ import { ApplicationRef, Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { concat, interval } from 'rxjs';
 import { first } from 'rxjs/operators';
+import { environment } from './../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class AppUpdateService {
-  constructor(appRef: ApplicationRef, swUpdate: SwUpdate) {
-    // https://medium.com/@arjenbrandenburgh/angulars-pwa-swpush-and-swupdate-15a7e5c154ac
-    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
-    const everySixHours$ = interval(6 * 60 * 60 * 1000);
-    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
-    everySixHoursOnceAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
+  constructor(appRef: ApplicationRef,
+              swUpdate: SwUpdate) {
+    if (environment.production) {
+      // https://medium.com/@arjenbrandenburgh/angulars-pwa-swpush-and-swupdate-15a7e5c154ac
+      const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+      const everySixHours$ = interval(6 * 60 * 60 * 1000);
+      const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+      everySixHoursOnceAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
+    }
   }
 }
