b04d4f7b036c32525524f11eac63252aae432830
Florian Gössele <moin@frot.io>
2019-06-16 20:57:38 +0200

    Added debug log messages

--- src/app/services/app-update/app-update.service.ts
+++ src/app/services/app-update/app-update.service.ts
@@ -9,11 +9,17 @@ export class AppUpdateService {
   constructor(appRef: ApplicationRef,
               swUpdate: SwUpdate) {
     if (environment.production) {
+      console.log('Checking for updates every six hours...');
       // https://medium.com/@arjenbrandenburgh/angulars-pwa-swpush-and-swupdate-15a7e5c154ac
       const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
       const everySixHours$ = interval(6 * 60 * 60 * 1000);
       const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
-      everySixHoursOnceAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
+      everySixHoursOnceAppIsStable$.subscribe(() => {
+        console.log('Checking for updates now.');
+        swUpdate.checkForUpdate();
+      });
+    } else {
+      console.log('No auto check for updates in dev mode');
     }
   }
 }
